a3d787d02e51f5ea540437d79dde9d35a23d6ec8
unknown <dim.min.6630@mail.ru>
2024-10-07 20:16:43 +0300

    Добавление документации и комментариев в проект

--- circle.py
+++ circle.py
@@ -2,9 +2,15 @@ import math
 
 
 def area(r):
+    '''
+    функция принимает радиус и возращает площадь круга
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    функция принимает радиус и возращает периметр круга
+    '''
     return 2 * math.pi * r
 
